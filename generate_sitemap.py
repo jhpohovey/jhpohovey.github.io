7d197a8457e9daeeba986bd3d662c72411f1882d
John Pohovey <42473764+jhpohovey@users.noreply.github.com>
2024-05-21 03:23:41 -0700

    actions sitemap gen attempt

---
+++ generate_sitemap.py
@@ -0,0 +1,26 @@
+import os
+from datetime import datetime
+
+BASE_URL = "https://jhpohovey.github.io/"
+lastmod = datetime.now().strftime("%Y-%m-%d")
+
+urls = [
+    BASE_URL,
+]
+
+sitemap_content = f'<?xml version="1.0" encoding="UTF-8"?>\n'
+sitemap_content += f'<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
+
+for url in urls:
+    sitemap_content += f'  <url>\n'
+    sitemap_content += f'    <loc>{url}</loc>\n'
+    sitemap_content += f'    <lastmod>{lastmod}</lastmod>\n' # globally across all pages should be fine
+    # part of the spec, but google ignores in crawl https://developers.google.com/search/docs/crawling-indexing/sitemaps/build-sitemap
+    sitemap_content += f'    <changefreq>monthly</changefreq>\n' 
+    sitemap_content += f'    <priority>1.0</priority>\n'
+    sitemap_content += f'  </url>\n'
+
+sitemap_content += f'</urlset>'
+
+with open("sitemap.xml", "w") as f:
+    f.write(sitemap_content)
